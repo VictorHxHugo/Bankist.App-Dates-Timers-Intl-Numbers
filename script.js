0ffa59e64fba1914508681026bfccc5cee87d290
VictorHxHugo <victorzk6@hotmail.com>
2023-12-29 22:02:36 -0300

    Numeric Separators

--- script.js
+++ script.js
@@ -329,6 +329,7 @@ console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
 */
 
+/*
 /////////////////////////////////////////////////
 // The Remainder Operator
 
@@ -356,3 +357,22 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+*/
+
+/////////////////////////////////////////////////
+// Numeric Separators
+
+const diameter = 287_450_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
