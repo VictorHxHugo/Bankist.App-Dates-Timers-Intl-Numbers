ce5b2eacbb8e94274745e9f47c9f763d18c2b07e
VictorHxHugo <victorzk6@hotmail.com>
2023-12-31 22:28:13 -0300

    Creating Dates

--- script.js
+++ script.js
@@ -379,6 +379,7 @@ console.log(Number('230_000'));
 console.log(parseInt('230_000'));
 */
 
+/*
 /////////////////////////////////////////////////
 // Working with BigInt
 
@@ -404,3 +405,22 @@ console.log(huge + ' is REALLY big!!!');
 // Divisions
 console.log(11n / 3n);
 console.log(10 / 3);
+*/
+
+///////////////////////////////////////
+// Creating Dates
+
+// Create a date
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
